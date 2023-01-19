3cf0aaa70c2e3dfddbce8a540c9a787084689682
Amanda Lee <leeandama87@gmail.com>
2023-01-18 16:13:52 -0800

    Added data to seeds.sql file using INSERT INTO. Created connecion.js inside of connection folder.

--- Develop/seeds/seeds.sql
+++ Develop/seeds/seeds.sql
@@ -1,8 +1,20 @@
-INSERT INTO department (name)
-VALUES (),
+INSERT INTO department (id, name)
+VALUES (01, 'Product'),
+       (02, 'Research'),
+       (03, 'Admin'),
+       (04, 'Marketing'),
+       (05, 'Development'),
 
-INSERT INTO role (name, department)
-VALUES (),
+INSERT INTO role (id, title, salary)
+VALUES (001, 'Engineer', 124000.00),
+       (010, 'Architect', 130000.00),
+       (090, 'Designer', 126000.00),
+       (170, 'Analyst', 120000.00),
+       (080, 'Tester', 112000.00),
        
-INSERT INTO employee (name)
-VALUES (),
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'Amanda', 45, 88),
+       (7, 'Kaylee', 47),
+       (6, 'Jade', 243),
+       (4, 'Bryan', 34, 2),
+       (5, 'Judah', 121),
